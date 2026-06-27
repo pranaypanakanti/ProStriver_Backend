3dc428fae3e4f8529da52cedf69e88e7d1d715c8
panakantipranay <panakantipranay@gmail.com>
2026-06-27 12:41:22 +0530

    Move com.prostriver to prostriver-app

--- prostriver-app/src/main/java/com/proStriver/ProStriverApplication.java
+++ prostriver-app/src/main/java/com/proStriver/ProStriverApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.ai.model.google.genai.autoconfigure.chat.GoogleGenAiChatAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@SpringBootApplication(
+        scanBasePackages = {"com.proStriver"},
+        exclude = {RedisRepositoriesAutoConfiguration.class, GoogleGenAiChatAutoConfiguration.class}
+)
+@EnableJpaRepositories(basePackages = "com.proStriver")
+@EntityScan(basePackages = {"com.proStriver", "com.springAi"})
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
-@SpringBootApplication(exclude = RedisRepositoriesAutoConfiguration.class)
 public class ProStriverApplication {
 
     public static void main(String[] args) {
@@ -14,3 +22,4 @@ public class ProStriverApplication {
     }
 
 }
+
